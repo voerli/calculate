c40543ac9fe229220aaaafd83ba6cba3e3ef4667
voerli <voerli@163.com>
2021-01-27 14:10:19 +0800

    等额本金还款优化

--- calculator/src/views/cal.vue
+++ calculator/src/views/cal.vue
@@ -3,30 +3,39 @@
     <div class="header">
       <div class="search">
         <el-row :gutter="gutter" class="rows">
-          <el-select v-model="value" placeholder="请选择分期类型">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            >
-            </el-option>
-          </el-select>
           <el-col :span="6"
             ><el-input v-model="total" placeholder="请输入本金"></el-input
           ></el-col>
           <el-col :span="6"
-            ><el-input v-model="rate" placeholder="请输入年利率"></el-input
+            ><el-input v-model="Yrate" placeholder="请输入年利率"></el-input
+          ></el-col>
+           <el-col :span="6"
+            ><el-input v-model="yearY" placeholder="请输入年限" @input="change"></el-input
           ></el-col>
           <el-col :span="6"
-            ><el-input v-model="year" placeholder="请输入期数"></el-input
+            ><el-input v-model="year" placeholder="请输入贷款期数"></el-input
           ></el-col>
         </el-row>
       </div>
-      <div class="btns">
-        <el-button type="primary">计算</el-button>
-        <el-button>清除</el-button>
+      <div class="btn-wrap">
+        <div class="btn-left">
+          <el-input v-model="number" placeholder="计算期数" /><el-button
+            type="primary"
+            @click="cal"
+            >利息相差额计算</el-button
+          >
+        </div>
+        <div class="btns">
+          <el-button type="primary" @click="count">计算</el-button>
+          <el-button @click="clear">清除</el-button>
+        </div>
       </div>
+    </div>
+    <div class="info" v-show="differ">
+      贷款<span>{{ year }}</span
+      >期，前<span>{{ number }}</span
+      >期等额本息和等额本金利息相差<span>{{ differ }}</span
+      >元
     </div>
     <div class="main">
       <div class="left">
@@ -37,7 +46,7 @@
           <el-table-column prop="pai" label="偿还本息"> </el-table-column>
           <el-table-column prop="interest" label="偿还利息"> </el-table-column>
           <el-table-column prop="principal" label="偿还本金"> </el-table-column>
-          <el-table-column prop="accumulation" label="利息累计">
+          <el-table-column prop="accumulation" label="利息累计" width="100">
           </el-table-column>
         </el-table>
       </div>
@@ -45,13 +54,11 @@
       <div class="right">
         <div class="l-title">等额本金</div>
         <el-table :data="tableDatas" style="width: 100%">
-          <el-table-column prop="date" label="期次" width="50">
-          </el-table-column>
-          <el-table-column prop="pai" label="偿还本息"> </el-table-column>
-          <el-table-column prop="interest" label="偿还利息"> </el-table-column>
-          <el-table-column prop="principal" label="偿还本金"> </el-table-column>
-          <el-table-column prop="accumulation" label="利息累计">
-          </el-table-column>
+          <el-table-column prop="accumulation" label="利息累计" width="100" />
+          <el-table-column prop="principal" label="偿还本金" />
+          <el-table-column prop="interest" label="偿还利息" />
+          <el-table-column prop="pai" label="偿还本息" />
+          <el-table-column prop="date" label="期次" width="50" />
         </el-table>
       </div>
     </div>
@@ -74,9 +81,13 @@ export default {
           label: "等额本金",
         },
       ],
-      total: 500000,
-      rate: 0.0588, //年利率
-      year: 3,
+      total: null || 500000,
+      Yrate: null || 0.0588,
+      rate: null, //年利率
+      yearY:null || 20,//贷款年限
+      year: null || 300,//贷款期数
+      number: null,
+      differ: null,
       tableData: [
         {
           accumulation: 0,
@@ -85,15 +96,12 @@ export default {
       tableDatas: [
         {
           accumulation: 0,
-          principal:0
+          // principal: 0,
         },
       ],
     };
   },
-  mounted() {
-    this.rate = this.rate / 12;
-    this.count();
-  },
+  mounted() {},
   // 等额本息计算公式
   // 每月还款额=贷款本金×[月利率×（1+月利率）^还款月数]÷[（1+月利率）^还款月数-1]
   // 总支付利息：总利息=还款月数×每月月供额-贷款本金
@@ -107,32 +115,56 @@ export default {
   // 每月月供递减额=每月应还本金×月利率=贷款本金÷还款月数×月利率
   // 总利息=还款月数×(总贷款额×月利率-月利率×(总贷款额÷还款月数)*(还款月数-1)÷2+总贷款额÷还款月数)
   methods: {
+    clear() {
+      this.total = null;
+      this.Yrate = null; //年利率
+      this.yearY = null;
+      this.year = null;
+      this.number = null;
+      this.differ = null;
+    },
+    change(){
+ if(this.yearY) this.year = this.yearY*12
+    },
     count() {
+      if (this.Yrate) this.rate = this.Yrate / 12;
+       this.tableData = [{accumulation: 0}];
+       this.tableDatas = [{accumulation: 0}];
       this.debj();
       this.debx();
     },
+    cal() {
+      this.count();
+      if(!this.number){
+        alert('请先填写计算期数')
+      }
+      this.differ =
+        (this.tableData[this.number].accumulation -
+        this.tableDatas[this.number].accumulation).toFixed(2);
+    },
     // 等额本金
     debj() {
+      let total = this.total;
+      //偿还本金
+      let principal = (total / this.year).toFixed(2);
       for (let i = 1; i <= this.year; i++) {
-        //剩余本金
-        let total = this.total
         // 每月月供额
-        let pai = (this.total / this.year + (total - 0) * this.rate).toFixed(2);
+        let pai = (
+          this.total / this.year +
+          (this.total - (this.total - total)) * this.rate
+        ).toFixed(2);
         //偿还利息
         let interest = (total * this.rate).toFixed(2);
-        //偿还本金
-        let principal = (total - this.year).toFixed(2);
-           //利息累计
-          let accumulation = (
+        total = total - principal;
+        //利息累计
+        let accumulation = (
           parseFloat(this.tableDatas[i - 1].accumulation) + parseFloat(interest)
         ).toFixed(2);
-        let obj = { date: i, pai, interest, principal,accumulation };
-     
+        let obj = { date: i, pai, interest, principal, accumulation };
         this.tableDatas.push(obj);
-        
       }
       this.tableDatas.splice(0, 1);
-    },/*  */
+    } /*  */,
     //等额本息
     debx() {
       //偿还本息
@@ -196,5 +228,26 @@ body {
   height: auto;
   background: #409eff;
   margin: 0 30px;
+}
+.btn-wrap {
+  display: flex;
+  justify-content: space-between;
+}
+.btn-left {
+  display: flex;
+}
+.btn-left button {
+  flex: 0 0 auto;
+  height: 40px;
+
+  line-height: 18px;
+  margin-left: 10px;
+}
+.info {
+  color: #999;
+  margin: 10px 0;
+}
+.info span {
+  color: #ff6600;
 }
 </style>
